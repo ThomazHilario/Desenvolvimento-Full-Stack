5668be56b3ab8cf1b305ba112dc67d4da4ecd37e
Thomaz Alves <thomazhilario5@gmail.com>
2023-09-16 10:20:31 -0300

    shuffle

--- Front-end/RPG0009 - Descobrindo o Java Script/1 - Ordenando com Java Script e DOM/ordenando.js
+++ Front-end/RPG0009 - Descobrindo o Java Script/1 - Ordenando com Java Script e DOM/ordenando.js
@@ -1,8 +1,24 @@
 // funções de ordenação
 
+//swap
 let swap = (vetor,idx1,idx2) => {
     const elementTemporario = vetor[idx1]
     vetor[idx1] = vetor[idx2]
     vetor[idx2] = elementTemporario
 }
+
+//shuffle
+let shuffle = (vetor) => {
+    // Armazenando o total de elementos de um array
+    let currentIndex = vetor.length
+    // Criando um for para percorrer um array
+    for(let i = 0; i < vetor.length;i++){
+        let randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--
+        // Chamando a função swap
+        swap(vetor,currentIndex,randomIndex)
+    }
+
+}
+
 
