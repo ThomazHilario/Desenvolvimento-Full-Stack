43e770a53c77a7c46af6348372ddbd6819f3440f
Thomaz Alves <thomazhilario5@gmail.com>
2023-10-09 13:56:59 -0300

    comentarios aos modelos

--- Front-end/RPG0010 - Meu primeiro framework/livros-next/classes/controle/controleEditora.ts
+++ Front-end/RPG0010 - Meu primeiro framework/livros-next/classes/controle/controleEditora.ts
@@ -1,17 +1,22 @@
 // ControleEditora.ts
 import Editora from '../modelo/editora';
 
+// array de editoras
 const editoras: Editora[] = [
   new Editora(1, 'Alta Books'),
   new Editora(2, 'Pearson'),
   new Editora(3, 'Addison Wesley'),
 ];
 
+// Class controleEditora
 class ControleEditora {
+
+  // metodo getEditoras
   getEditoras(): Editora[] {
     return editoras;
   }
 
+  // metodo getNomeEditora
   getNomeEditora(codEditora: number): string {
     const editora = editoras.find((e) => e.codEditora === codEditora);
     return editora ? editora.nome : '';
