fe00bd94ea534b39f68000da8317ce7654d98473
Thomaz Alves <thomazhilario5@gmail.com>
2023-10-24 13:16:25 -0300

    configurando o livro dao

---
+++ Front-end/RPG0012 - Vamos colocar o framework para acessar um banco/livro-servidor/modelo/livro-dao.js
@@ -0,0 +1,35 @@
+// import livro schema
+const Livro = require('./livro-schema')
+
+// obtendo livros
+const obterLivros = async () => {
+    try {
+        const livros = await Livro.find()
+        return livros
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// incluir livro
+const incluir = async (livro) =>{
+    try {
+        const novoLivro = new Livro(livro)
+        const livroInserido = await novoLivro.save()
+        return livroInserido
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// excluindo livro
+const excluir = async (codigo) => {
+    try {
+        await Livro.deleteOne({ _id: codigo })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// exportando 
+module.exports = { obterLivros, incluir, excluir };
