d3b7939cfcf91cf48797821037f5f95e258839c5
Thomaz Alves <thomazhilario5@gmail.com>
2024-02-13 14:29:40 -0300

    Criando codigo em java

---
+++ Back-End-Java/Estudos/Entendendo-Java/Introduction/src/Pessoa.java
@@ -0,0 +1,15 @@
+public class Pessoa{
+    // Atributos
+    private String name;
+    private int age;
+    private String data_nascimento;
+    private String cpf;
+
+    // Constructor
+    public Pessoa(String name, int age, String data_nascimento, String cpf){
+        this.name = name;
+        this.age = age;
+        this.data_nascimento = data_nascimento;
+        this.cpf = cpf;
+    }
+}
