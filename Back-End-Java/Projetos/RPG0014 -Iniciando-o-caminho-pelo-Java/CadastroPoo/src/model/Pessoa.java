a9bc7f3cd025dce4094cab724b848f4ad57ff523
Thomaz Alves <thomazhilario5@gmail.com>
2024-02-19 10:02:26 -0300

    Criando a classe pessoaFisica e adicionando seriazable

--- Back-End-Java/Projetos/RPG0014 -Iniciando-o-caminho-pelo-Java/CadastroPoo/src/model/Pessoa.java
+++ Back-End-Java/Projetos/RPG0014 -Iniciando-o-caminho-pelo-Java/CadastroPoo/src/model/Pessoa.java
@@ -1,6 +1,6 @@
 package model;
 
-public class Pessoa {
+public class Pessoa{
     // atributos
     private Integer id;
     private String nome;
--- Back-End-Java/Projetos/RPG0014 -Iniciando-o-caminho-pelo-Java/CadastroPoo/src/model/PessoaFisica.java
+++ Back-End-Java/Projetos/RPG0014 -Iniciando-o-caminho-pelo-Java/CadastroPoo/src/model/PessoaFisica.java
@@ -1,5 +1,41 @@
 package model;
 
-public class PessoaFisica extends Pessoa{
-    
+import java.io.Serializable;
+
+public class PessoaFisica extends Pessoa implements Serializable{
+    // Atributos
+    private String cpf;
+    private Integer idade;
+
+    // Constructor
+    PessoaFisica(String nome, Integer id, String cpf, Integer idade){
+        super(id, nome);
+
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public String getCpf(){
+        return this.cpf;
+    }
+
+    public void setIdade(Integer idade){
+        this.idade = idade;
+    }
+
+    public Integer getIdade(){
+        return this.idade;
+    }
+
+    @Override
+    public void exibir() {
+        System.out.println("Nome: " + getName());
+        System.out.println("Id: " + getId());
+        System.out.println("Idade: " + getIdade());
+        System.out.println("Cpf: " + getCpf());
+    }
 }
