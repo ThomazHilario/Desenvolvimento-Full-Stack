7a1d80ddac5f208623bab119784cdda9a0eefba4
Thomaz Alves <thomazhilario5@gmail.com>
2024-02-21 10:20:45 -0300

    Criando a classe principal

---
+++ Back-End-Java/Projetos/RPG0014 -Iniciando-o-caminho-pelo-Java/CadastroPoo/src/model/CadastroPoo.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.Scanner;
+
+public class CadastroPoo {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        PessoaFisicaRepo repoFisica = new PessoaFisicaRepo();
+    }
+}
